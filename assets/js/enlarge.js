bd8e69b066efbec5a74940e549dcbb8d8789b3c0
Kevin Masterson <k.m.masterson@gmail.com>
2026-05-08 18:58:10 +0300

    add and use webp version of avatar-icon for faster load times. minify some heavy local css files, and add a minified enlarge.js

--- assets/js/enlarge.js
+++ assets/js/enlarge.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
 	});
 
 	Array.from(document.getElementsByClassName("col-xl-8")).forEach(element => {
-	  element.classList.add("col-xl-10");
+	  // element.classList.add("col-xl-10");
 	  element.classList.remove("col-xl-8");
 	});
 
 	Array.from(document.getElementsByClassName("offset-xl-2")).forEach(element => {
-	  element.classList.add("offset-xl-1");
+	  // element.classList.add("offset-xl-1");
 	  element.classList.remove("offset-xl-2");
 	});
 });
