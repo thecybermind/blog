19beeaa64951f87441f987d3a887f76e6d4f497c
Kevin Masterson <k.m.masterson@gmail.com>
2026-05-08 06:19:54 +0300

    Add enlarge.js to resize containers without needing to manually override the layouts. Add feed.xml and RSS link in footer. Enable post_search and add keywords. Add "mathjax: true" to posts.

---
+++ assets/js/enlarge.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	Array.from(document.getElementsByClassName("container-md")).forEach(element => {
+	  element.classList.add("container-xl");
+	  element.classList.remove("container-md");
+	});
+
+	Array.from(document.getElementsByClassName("col-xl-8")).forEach(element => {
+	  element.classList.add("col-xl-10");
+	  element.classList.remove("col-xl-8");
+	});
+
+	Array.from(document.getElementsByClassName("offset-xl-2")).forEach(element => {
+	  element.classList.add("offset-xl-1");
+	  element.classList.remove("offset-xl-2");
+	});
+});
